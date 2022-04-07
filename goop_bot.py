42c1d75fd754518c4e9624306321e56107301448
IvanY17104 <99291681+IvanY17104@users.noreply.github.com>
2022-04-07 15:22:46 +1200

    Added comments for each major section
    Comments

--- goop_bot.py
+++ goop_bot.py
@@ -1,6 +1,56 @@
 #This will be a program for an online store that will sell goop
 
+#Welcome message with random name   
 
+
+
+
+#Menu for pickup or delivery
+
+
+
+
+#Pickup information - name and phone number
+
+
+
+
+#Delivery Infornation - name address and phone
+
+
+
+
+#Choose total number of items - max 5
+
+
+
+
+#Goop Menu
+
+
+
+
+#Goop Ordering - from menu - print each item with cost
+
+
+
+
+#Print order out - including if order is delivery or pickup and names and price of each item - total cost
+
+
+
+
+#Ability to cancel or proceed with order
+
+
+
+
+#Create option for new order or to exit
+
+
+
+
+#Main Funciton
 
 
 
