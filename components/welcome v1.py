b7a963b73a6498215349150bcb635a5265b0f9d6
IvanY17104 <99291681+IvanY17104@users.noreply.github.com>
2022-04-07 14:11:29 +1200

    Created welcome component
    Created a welcome message with a random name, and made it into a function.
    
    v1 and v2

---
+++ components/welcome v1.py
@@ -0,0 +1,14 @@
+#Welcome message and Random name genrator component
+#Creates a random name from a random name generator
+#Prints a welcome message that includes the random name
+
+import random
+from random import randint #this import allows me to create a random number generator
+
+names = ["Ivan", "James", "Matthew", "Ryan", "Felix", "Grace", "Amy", "Alice", "Lara",  "Rayyan"]#List of random names
+
+num = randint(0,9) #num will give a random number (randint) between 0 to 9
+
+name = (names[num]) #each name in names has a number associated with it, and num  will give a random number, so 'name' will give a random name
+print("Welcome to the Goop Store!")
+print("My name is",name,"and I will help you take your Goop orders.") #This is the welcome message that will show up
---
+++ goop_bot.py
@@ -0,0 +1,6 @@
+#This will be a program for an online store that will sell goop
+
+
+
+
+
