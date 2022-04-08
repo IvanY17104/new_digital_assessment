7d7397259a51d781fae324e0326f60b74cbbedd5
IvanY17104 <99291681+IvanY17104@users.noreply.github.com>
2022-04-08 14:11:32 +1200

    Small changes
    Integrated pickup menu into main app, added folders for components

--- goop_bot.py
+++ goop_bot.py
@@ -22,7 +22,32 @@ def welcome(): #Created a function for welcome
 
 
 #Menu for pickup or delivery
+def pickup():
+    print ("Do you want your order delivered or are you picking it up?")
+    print ("For delivery enter 1")
+    print ("For pickup enter 2")#Asks the users what they want
 
+    low = 1#lowest number user can input is 1
+    high = 2#highest number user can input is 2
+
+    while True:#this creates a loop, so things are repeated if there are 
+        try:
+            delivery = int(input("Please enter a number "))#delivery allows integer inputs
+            
+            if delivery >= 1 and delivery <= 2:
+
+                if delivery == 1:
+                    print("Delivery")#if 1 is inputted it prints delivery
+                    break#breaks loop
+
+                elif delivery == 2:
+                    print("Pickup")#if 2 is inputted it prints pickup
+                    break#breaks loop
+            else:
+                print("The number must be 1 or 2")#prints this when a number is less than 1 or greater than 2 
+
+        except ValueError:
+            print("That is not a valid number, please enter 1 or 2")#this prints when expected values arent inputted
 
 
 
@@ -67,13 +92,14 @@ def welcome(): #Created a function for welcome
 
 
 #Main Funciton
-def main(): #Created a function to run welcome
+def main(): #Created a function to run all the functions
     '''
     Purpose: To run all functions
     Parameters: none
     Returns: none
     '''
     welcome()
+    pickup()
 
 main() #Runs the main function 
 
