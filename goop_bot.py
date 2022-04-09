de8531f01de016ae034190dbd60621da96942a47
IvanY17104 <17104@my.sanctamaria.school.nz>
2022-04-09 14:42:40 +1200

    Updated goop_bot
    Integrated pickup function and make tweaks to code

--- goop_bot.py
+++ goop_bot.py
@@ -5,6 +5,18 @@ from random import randint #this import allows me to create a random number gene
 
 names = ["Ivan", "James", "Matthew", "Ryan", "Felix", "Grace", "Amy", "Alice", "Lara",  "Rayyan"]#List of random names
 
+customer_details = {}#a dictionary to store customer details
+
+#validates inputs to check if they are blank
+def not_blank(question):#a not blank function to check if something isnt blank
+    valid = False
+    while not valid:#a loop
+        response = input(question)#asks the question that is stated, and allows input
+        if response != "":
+            return response.title()#if input isn't blank returns it back, and it makes the input's first letter have capital and the rest lower case
+        else:
+            print("Sorry this cannot be blank")#if input is blank this is printed
+
 #Welcome message with random name   
 def welcome(): #Created a function for welcome
     '''
@@ -22,7 +34,7 @@ def welcome(): #Created a function for welcome
 
 
 #Menu for pickup or delivery
-def pickup():
+def order_type():
     print ("Do you want your order delivered or are you picking it up?")
     print ("For delivery enter 1")
     print ("For pickup enter 2")#Asks the users what they want
@@ -42,6 +54,7 @@ def pickup():
 
                 elif delivery == 2:
                     print("Pickup")#if 2 is inputted it prints pickup
+                    pickup()#will activate pickup function and begin asking things for pickup
                     break#breaks loop
             else:
                 print("The number must be 1 or 2")#prints this when a number is less than 1 or greater than 2 
@@ -52,7 +65,15 @@ def pickup():
 
 
 #Pickup information - name and phone number
+def pickup():
+    question = ("Please enter your name ")#asks the user to enter their name
+    customer_details['name'] =  not_blank(question)#customer_details goes off to not_blank, later not_blank is sent back to customer_details after input
 
+    question = ("Please enter your phone number ")#asks the user to enter theirn phone number
+    customer_details['phone'] = not_blank(question)#same as above
+
+    print(customer_details['name'])
+    print(customer_details['phone'])#prints the 2 inputs stored in dictionary
 
 
 
@@ -99,7 +120,8 @@ def main(): #Created a function to run all the functions
     Returns: none
     '''
     welcome()
-    pickup()
+    order_type()
+
 
 main() #Runs the main function 
 
