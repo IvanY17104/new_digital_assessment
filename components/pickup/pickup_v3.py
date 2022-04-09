9b08ad4d68a5800a9cde226506a32ddd837b4a68
IvanY17104 <99291681+IvanY17104@users.noreply.github.com>
2022-04-09 13:11:49 +1200

    Small changes
    commit so i can work from another computer

--- components/pickup/pickup_v3.py
+++ components/pickup/pickup_v3.py
@@ -1,24 +1,23 @@
-customer_detail = {}#a dictionary to store
+customer_details = {}#a dictionary to store customer details
 
 print("Please enter the pickup information")#asks the user to enter their information
 
 valid = False
 while not valid:#this along with line above creates a loop
-    customer_detail['name'] = input("Please enter your name ")#asks for user name and allows input
-    if customer_detail['name'] != "":#if name isn't blank
-        print (customer_detail['name'])
+    customer_details['name'] = input("Please enter your name ")#asks for user name and allows input
+    if customer_details['name'] != "":#if name isn't blank
+        print (customer_details['name'])
         break#ends loop
     else:
         print("Sorry this cannot be blank")#this prints if name is blank
 
 valid = False
 while not valid:#loop
-    customer_detail['phone'] = input("Please enter your phone number")#asks for user phone number and allows input
-    if customer_detail['phone'] != "":#if phone isn't blank
-        print(customer_detail['phone'])
+    customer_details['phone'] = input("Please enter your phone number")#asks for user phone number and allows input
+    if customer_details['phone'] != "":#if phone isn't blank
+        print(customer_details['phone'])
         break#ends loop
     else:
         print("Sorry this can't be blank")#this prints if phone is blank
- 
-print(customer_detail['name'])
-print(customer_detail['phone'])#prints the name and phone number
+
+print(customer_details)
